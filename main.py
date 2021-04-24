8f4a4646a82fa22008c91991bb111ae2a73910f2
Pacil142857 <7coolaustin7@gmail.com>
2021-04-24 16:16:33 -0400

    Any files in the input directory with parent folders keep their folders in the output directory. The code has also been cleaned, and the output and tmp directories are deleted at the start of the program.

--- main.py
+++ main.py
@@ -3,8 +3,32 @@ import subprocess
 from pydub import AudioSegment
 from shutil import rmtree
 
+# Delete tmp directory
+if os.path.exists(r'.\tmp') and os.path.isdir(r'.\tmp'):
+    rmtree(r'.\tmp')
+
+# Empty output directory
+if os.path.exists(r'.\output') and os.path.isdir(r'.\output'):
+    rmtree(r'.\output')
+os.mkdir('output')
+
+class File:
+    '''Represents a file. Contains the path, filename, and file extension.'''
+    def __init__(self, path):
+        self.path = path
+        
+        # Get filename and extension
+        self.filename = path[path.rfind('\\') + 1:]
+        self.file_ext = self.filename[self.filename.rfind('.'):]
+        self.filename = self.filename[:self.filename.rfind('.')]
+        
+        # Get the path, sans the first two directories (usually ".\input\" or ".\tmp\")
+        # This also doesn't include the file extension, unlike self.path
+        self.rel_path = '\\'.join(path.split('\\')[2:])
+        self.rel_path = self.rel_path[:self.rel_path.rfind('.')]
+
 # Find every file (recursively) in input
-nus3audio_files = [os.path.join(path, file) for path, _, files in os.walk(r'.\input') for file in files]
+nus3audio_files = [File(os.path.join(path, file)) for path, _, files in os.walk(r'.\input') for file in files]
 
 # These lists get used later in the program
 lopus_files = []
@@ -25,10 +49,11 @@ for file in (r'.\input\.gitkeep', r'.\output\.gitkeep'):
 # Extract lopus files from the nus3audio files in input
 for file in nus3audio_files:
     
-    # Get the filename and file extension
-    filename = file[file.rfind('\\') + 1:]
-    file_ext = file[file.rfind('.'):]
-    filename = filename[:filename.rfind('.')]
+    # Get the filename, extension, and path
+    filename = file.filename
+    file_ext = file.file_ext
+    path = file.path
+    parents = file.rel_path[:-1 * len(filename)]
     
     # Skip file if file doesn't end in nus3audio
     if file_ext != '.nus3audio':
@@ -36,20 +61,20 @@ for file in nus3audio_files:
         continue
     
     # Convert the file and store it in the tmp directory
-    command = [r'.\tools\nus3audio.exe', file, '-e', r'.\tmp']
+    command = [r'.\tools\nus3audio.exe', path, '-e', '.\\tmp\\' + parents]
     old_filename = subprocess.run(command, stdout=subprocess.PIPE).stdout
     old_filename = old_filename.decode('utf-8')[:-1]
     
-    new_filepath = '.\\tmp\\' + filename + '.lopus'
+    new_filepath = '.\\tmp\\' + file.rel_path + '.lopus'
     
     # Rename the lopus file to the filename of the nus3audio file
     try:
-        os.rename('.\\tmp\\' + old_filename, new_filepath)
+        os.rename('.\\tmp\\' + parents + old_filename, new_filepath)
     except FileExistsError:
         os.remove(new_filepath)
         os.rename('.\\tmp\\' + old_filename, new_filepath)
         
-    lopus_files.append(new_filepath)
+    lopus_files.append(File(new_filepath))
 
 # Load settings
 with open('config.txt') as f:
@@ -68,24 +93,30 @@ with open('config.txt') as f:
 
 # Convert lopus files to wav files
 for file in lopus_files:
-    # Get the filename and the new file path
-    filename = file[file.rfind('\\') + 1:-6]
-    new_filepath = '.\\tmp\\' + filename + '.wav'
+    # Get the filename and some other things
+    filename = file.filename
+    new_filepath = '.\\tmp\\' + file.rel_path + '.wav'
+    path = file.path
     
     # Convert the lopus file to a wav file
-    command = [r'.\tools\vgmstream\test.exe', '-l', num_loops, '-f', fade_duration, '-o', new_filepath, file]
+    command = [r'.\tools\vgmstream\test.exe', '-l', num_loops, '-f', fade_duration, '-o', new_filepath, path]
     subprocess.run(command)
     
     # Add the new file to wav_files
-    wav_files.append(new_filepath)
+    wav_files.append(File(new_filepath))
 
+os.chdir(r'.\output')
 # Convert wav files to flac files
 for file in wav_files:
-    # Get the filename
-    filename = file[file.rfind('\\') + 1:-4]
-    
+    # Create folders (if needed)
+    folders = file.rel_path[:-1 * len(file.filename)]
+    if folders:
+        try:
+            os.makedirs(folders)
+        except FileExistsError:
+            pass
     # Convert the file
-    subprocess.run([r'.\tools\sox\sox.exe', file, '.\\output\\' + filename + '.flac'])
+    subprocess.run([r'..\tools\sox\sox.exe', '.' + file.path, '..\\output\\' + file.rel_path + '.flac'])
 
 # Delete the tmp folder
-rmtree('tmp')
+rmtree('..\\tmp')
