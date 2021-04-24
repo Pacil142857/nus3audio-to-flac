8206a228129e65c3126c0f266c49dd3eef969e67
Pacil142857 <7coolaustin7@gmail.com>
2021-04-24 10:16:09 -0400

    Created a basic outline of the program

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+from pydub import AudioSegment
+
+# Extract lopus files from the nus3audio files in input
+
+# Convert lopus files to wav files
+
+# Convert wav files to flac files
+# Example:
+# song = AudioSegment.from_wav('foo.wav')
+# song.export('bar.flac', format='flac')
